72f2b460377fc81a5270adbb1d3ea06410f74d12
Edgar Coronel <edgarcoronel66@gmail.com>
2024-10-07 17:43:09 -0700

    Se encapsula el componente DataTableBootstrap para intentar hacer que el bootstrap solo afecte a este componente, no se logró, aunque si hay algunas mejoras, también se coloca el favicon correcto

---
+++ src/components/DataTableBootstrap.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted } from 'vue';
+import DataTablesCore from 'datatables.net-bs5';
+import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css';
+
+let dataTableInstance: any = null;
+
+onMounted(() => {
+  // Iniciar la tabla cuando se monta el componente
+  if (!dataTableInstance) {
+    dataTableInstance = new DataTablesCore('#customTable');
+  }
+});
+
+onUnmounted(() => {
+  // Destruir la instancia de DataTable cuando el componente se desmonte
+  if (dataTableInstance) {
+    dataTableInstance.destroy();
+    dataTableInstance = null;
+  }
+});
+</script>
+
+<template>
+  <table id="customTable" class="table table-hover table-striped">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Nombre</th>
+        <th>Fecha Registro</th>
+        <th>Edad</th>
+        <th>Sexo</th>
+        <th>Escolaridad</th>
+        <th>Puesto</th>
+        <th>Antigüedad</th>
+        <th>Teléfono</th>
+        <th>Estado Civil</th>
+        <th>Hijos</th>
+        <th>Expediente</th>
+        <th>-</th>
+      </tr>
+    </thead>
+    <tbody>
+      <slot></slot> <!-- Permitir que el contenido (filas de trabajadores) sea pasado desde el componente padre -->
+    </tbody>
+  </table>
+</template>
+
+<style scoped>
+@import 'bootstrap/dist/css/bootstrap.min.css'; /* Importar Bootstrap de manera encapsulada */
+
+.pagination {
+  --bs-pagination-padding-x: 0.75rem;
+  --bs-pagination-padding-y: 0.375rem;
+  --bs-pagination-font-size: 1rem;
+  --bs-pagination-color: #105a38 !important;
+  --bs-pagination-bg: var(--bs-body-bg);
+  --bs-pagination-border-width: var(--bs-border-width);
+  --bs-pagination-border-color: var(--bs-border-color);
+  --bs-pagination-border-radius: var(--bs-border-radius);
+  --bs-pagination-hover-color: #105a38 !important;
+  --bs-pagination-hover-bg: var(--bs-tertiary-bg);
+  --bs-pagination-hover-border-color: var(--bs-border-color);
+  --bs-pagination-focus-color: #105a38 !important;
+  --bs-pagination-focus-bg: var(--bs-secondary-bg);
+  --bs-pagination-focus-box-shadow: 0 0 0 0.25rem rgba(26, 136, 81, 0.25) !important;
+  --bs-pagination-active-color: #fff;
+  --bs-pagination-active-bg: #198754 !important;
+  --bs-pagination-active-border-color: #198754 !important;
+  --bs-pagination-disabled-color: var(--bs-secondary-color);
+  --bs-pagination-disabled-bg: var(--bs-secondary-bg);
+  --bs-pagination-disabled-border-color: var(--bs-border-color);
+}
+</style>
--- src/views/TrabajadoresView.vue
+++ src/views/TrabajadoresView.vue
@@ -1,117 +1,79 @@
 <script setup lang="ts">
-import { onMounted, onUnmounted } from 'vue';
+import { onMounted } from 'vue';
 import { useTrabajadoresStore } from '@/stores/trabajadores';
 import { useCentrosTrabajoStore } from '@/stores/centrosTrabajo';
 import { useRoute, useRouter } from 'vue-router';
 import { convertirFechaISOaDDMMYYYY, calcularEdad, calcularAntiguedad } from '@/helpers/dates';
 import GreenButton from '@/components/GreenButton.vue';
-import DataTablesCore from 'datatables.net-bs5';
-import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css';
+import DataTableBootstrap from '@/components/DataTableBootstrap.vue';
 
 const trabajadores = useTrabajadoresStore();
 const centrosTrabajo = useCentrosTrabajoStore();
 const route = useRoute();
 const router = useRouter();
-let dataTableInstance: any = null;
 
 onMounted(() => {
   const empresaId = String(route.params.idEmpresa);
   const centroTrabajoId = String(route.params.idCentroTrabajo);
-  trabajadores.fetchTrabajadores(empresaId, centroTrabajoId)
+  trabajadores.fetchTrabajadores(empresaId, centroTrabajoId);
 
   // Setear el ID del centro de trabajo actual en el store
   centrosTrabajo.currentCentroTrabajoId = centroTrabajoId;
   centrosTrabajo.fetchCentroTrabajoById(empresaId, centroTrabajoId);
-  console.log(trabajadores.trabajadores)
-
-  setTimeout(() => {
-    if (!dataTableInstance) {
-      dataTableInstance = new DataTablesCore('#customTable');
-    }
-  }, 1000); // Tiempo para esperar la carga de datos
-
-});
-
-onUnmounted(() => {
-  if (dataTableInstance) {
-    dataTableInstance.destroy(); // Destruir instancia de DataTable cuando el componente se desmonte
-    dataTableInstance = null;
-  }
 });
 </script>
 
 <template>  
-<div class="w-full p-5 space-y-5">
+  <div class="w-full p-5 space-y-5">
     <div class="flex flex-col md:flex-row justify-center gap-3 md:gap-8">
       <GreenButton text="Nuevo Trabajador +" />
       <GreenButton text="Carga Masiva" />
       <GreenButton text="Exportar a Excel" />
     </div>
+
     <Transition appear mode="out-in" name="slide-up">
       <div v-if="trabajadores.loading">
         <h1 class="text-3xl sm:text-4xl md:text-6xl py-20 text-center font-semibold text-gray-700">
           Cargando...
         </h1>
       </div>
-      <div v-else>  
-        <table v-if="trabajadores.trabajadores.length > 0" id="customTable" class="table table-hover table-striped">
-            <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Nombre</th>
-                  <th>Fecha Registro</th>
-                  <th>Edad</th>
-                  <th>Sexo</th>
-                  <th>Escolaridad</th>
-                  <th>Puesto</th>
-                  <th>Antigüedad</th>
-                  <th>Teléfono</th>
-                  <th>Estado Civil</th>
-                  <th>Hijos</th>
-                  <th>Expediente</th>
-                  <th>-</th>
-                </tr>
-            </thead>
-            <tbody>
-              <tr v-for="(trabajador, index) in trabajadores.trabajadores" :key="trabajador._id">
-                  <td>{{ index + 1 }}</td>
-                  <td>{{ trabajador.nombre }}</td>
-                  <td>{{ convertirFechaISOaDDMMYYYY(trabajador.createdAt) }}</td>
-                  <td>{{ calcularEdad(trabajador.fechaNacimiento)}}</td>
-                  <td>{{ trabajador.sexo }}</td>
-                  <td>{{ trabajador.escolaridad }}</td>
-                  <td>{{ trabajador.puesto }}</td>
-                  <td>{{ calcularAntiguedad(trabajador.fechaIngreso) }}</td>
-                  <td>{{ trabajador.telefono ? trabajador.telefono : '-' }}</td>
-                  <td>{{ trabajador.estadoCivil }}</td>
-                  <td>{{ trabajador.hijos }}</td>
-                  <td>
-                    <button 
-                      type="button" 
-                      class="bg-emerald-600 hover:bg-emerald-700 hover:scale-105 text-white rounded-lg px-2 py-1 transition-all duration-300 ease-in-out transform"
-                      @click="router.push({ name: 'expediente-medico', params: { idEmpresa: trabajador._id, idCentroTrabajo: centrosTrabajo.currentCentroTrabajoId, idTrabajador: trabajador._id } })"
-                    >
-                        Expediente
-                    </button>
-                  </td>
-                  <td>
-                      <div class="flex gap-1">
-                          <button type="button" class="hover:scale-110 transition-all duration-100 ease-in-out transform">
-                            <i class="fa-regular fa-pen-to-square fa-lg" style="color: #696969" ></i>
-                          </button>
-                          <button type="button" class="hover:scale-110 transition-all duration-100 ease-in-out transform">
-                            <i class="fa-solid fa-trash-can fa-lg" style="color: #c43117"></i>
-                          </button>
-                      </div>
-                  </td>
-                </tr>
-              </tbody>
-        </table>
+      <div v-else>
+        <!-- Usar el componente DataTableBootstrap -->
+        <DataTableBootstrap>
+          <tr v-for="(trabajador, index) in trabajadores.trabajadores" :key="trabajador._id">
+            <td>{{ index + 1 }}</td>
+            <td>{{ trabajador.nombre }}</td>
+            <td>{{ convertirFechaISOaDDMMYYYY(trabajador.createdAt) }}</td>
+            <td>{{ calcularEdad(trabajador.fechaNacimiento)}}</td>
+            <td>{{ trabajador.sexo }}</td>
+            <td>{{ trabajador.escolaridad }}</td>
+            <td>{{ trabajador.puesto }}</td>
+            <td>{{ calcularAntiguedad(trabajador.fechaIngreso) }}</td>
+            <td>{{ trabajador.telefono ? trabajador.telefono : '-' }}</td>
+            <td>{{ trabajador.estadoCivil }}</td>
+            <td>{{ trabajador.hijos }}</td>
+            <td>
+              <button 
+                type="button" 
+                class="bg-emerald-600 hover:bg-emerald-700 hover:scale-105 text-white rounded-lg px-2 py-1 transition-all duration-300 ease-in-out transform"
+                @click="router.push({ name: 'expediente-medico', params: { idEmpresa: trabajador._id, idCentroTrabajo: centrosTrabajo.currentCentroTrabajoId, idTrabajador: trabajador._id } })"
+              >
+                Expediente
+              </button>
+            </td>
+            <td>
+              <div class="flex gap-1">
+                <button type="button" class="hover:scale-110 transition-all duration-100 ease-in-out transform">
+                  <i class="fa-regular fa-pen-to-square fa-lg" style="color: #696969"></i>
+                </button>
+                <button type="button" class="hover:scale-110 transition-all duration-100 ease-in-out transform">
+                  <i class="fa-solid fa-trash-can fa-lg" style="color: #c43117"></i>
+                </button>
+              </div>
+            </td>
+          </tr>
+        </DataTableBootstrap>
       </div>
-    </Transition>   
+    </Transition>
   </div>
 </template>
-
-<style>
-@import 'bootstrap/dist/css/bootstrap.min.css';
-</style>
