6c35d08d9face6a534862d3421d02f90656d0c49
Edgar Coronel <edgarcoronel66@gmail.com>
2024-11-07 09:09:21 -0700

    Se comienza a experimentar con el layout de los documentos en la vista expedientes

--- src/views/ExpedienteMedicoView.vue
+++ src/views/ExpedienteMedicoView.vue
@@ -5,6 +5,7 @@ import { useEmpresasStore } from '@/stores/empresas';
 import { useCentrosTrabajoStore } from '@/stores/centrosTrabajo';
 import { useTrabajadoresStore } from '@/stores/trabajadores';
 import GreenButton from '@/components/GreenButton.vue';
+import { calcularEdad } from '@/helpers/dates';
 
 const route = useRoute();
 const router = useRouter();
@@ -36,22 +37,34 @@ watch(
 <template>
   <div class="p-5 grid gap-5">
     <div class="flex flex-col md:flex-row justify-center gap-3 md:gap-6">
-      <GreenButton text="Historia Clínica" />
-      <GreenButton text="Exploración Fisica" />
-      <GreenButton text="Examen Vista" />
-      <GreenButton text="Antidoping" />
-      <GreenButton text="Aptitud" />
-      <GreenButton text="Certificado" />
+      <GreenButton class="text-base sm:text-lg md:text-lg lg:text-lg xl:text-xl 2xl:text-xl" text="Historia Clínica" />
+      <GreenButton class="text-base sm:text-lg md:text-lg lg:text-lg xl:text-xl 2xl:text-xl" text="Exploración Fisica" />
+      <GreenButton class="text-base sm:text-lg md:text-lg lg:text-lg xl:text-xl 2xl:text-xl" text="Examen Vista" />
+      <GreenButton class="text-base sm:text-lg md:text-lg lg:text-lg xl:text-xl 2xl:text-xl" text="Antidoping" />
+      <GreenButton class="text-base sm:text-lg md:text-lg lg:text-lg xl:text-xl 2xl:text-xl" text="Aptitud" />
+      <GreenButton class="text-base sm:text-lg md:text-lg lg:text-lg xl:text-xl 2xl:text-xl" text="Certificado" />
     </div>
     <Transition appear mode="out-in" name="slide-up">
-      <div class="w-full rounded-lg p-2 shadow-lg bg-gray-50 items-center grid grid-cols-1 gap-8">
-        <h1>Expediente Médico de {{ trabajadores.currentTrabajador?.nombre }}</h1>
-        <h1>Historia Clinica</h1>
-        <h1>Exploracion Fisica</h1>
-        <h1>Antidoping</h1>
-        <h1>Examen de la Vista</h1>
-        <h1>Aptitud al Puesto</h1>
+      <div v-if="trabajadores.currentTrabajador" class="w-full text-center items-center grid grid-cols-1 gap-1 mt-4">
+        <h1 class="text-3xl">{{ trabajadores.currentTrabajador?.nombre }}</h1>
+        <h1 class="text-xl">{{ calcularEdad(trabajadores.currentTrabajador?.fechaNacimiento) }} años - {{ trabajadores.currentTrabajador?.puesto }}</h1>
       </div>
     </Transition>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div class="w-full">
+        <h1 class="border-2 border-gray-800 bg-gray-300 rounded-t-lg text-center">Documentos 2025</h1>
+        <h1 class="border-2 border-gray-800 border-t-0 bg-gray-50">item 2</h1>
+        <h1 class="border-2 border-gray-800 border-t-0 bg-gray-50">item 3</h1>
+      </div>
+      <div class="w-full">
+        <h1 class="border-2 border-gray-800 bg-gray-300 rounded-t-lg text-center">Documentos 2024</h1>
+        <div class="border-2 border-gray-800 border-t-0 bg-gray-50">
+          <p class="text-lg p-1">Aptitud al Puesto <span class="text-sm">14-11-2024</span></p>
+          <p class="text-sm px-1">Apto Sin Restricciones</p>
+        </div>
+        <h1 class="border-2 border-gray-800 border-t-0 bg-gray-50">item 3</h1>
+      </div>
+    </div>
+
   </div>
 </template>
