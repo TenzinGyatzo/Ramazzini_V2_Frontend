442ad86cfe5e7aeca37172d94e02e18510b9d2d7
Edgar Coronel <edgarcoronel66@gmail.com>
2025-03-12 19:55:09 -0700

    Se modifica el sistema de planes y precios

--- src/views/SubscriptionView.vue
+++ src/views/SubscriptionView.vue
@@ -27,9 +27,9 @@ const router = useRouter();
 
 // Planes disponibles
 const plans = [
-  { id: 1, name: "Básico", price: 599, histories: 50, maxHistories: 225, nextPlanPrice: 1999 },
-  { id: 2, name: "Profesional", price: 1999, histories: 300, maxHistories: 275, nextPlanPrice: 3899 },
-  { id: 3, name: "Empresarial", price: 3899, histories: 600, maxHistories: 5000 },
+  { id: 1, name: "Básico", price: 699, histories: 50, maxHistories: 100, nextPlanPrice: 1399 },
+  { id: 2, name: "Profesional", price: 1399, histories: 150, maxHistories: 150, nextPlanPrice: 2499 },
+  { id: 3, name: "Empresarial", price: 2499, histories: 300, maxHistories: 5000 },
 ];
 // const plans = [
 //   { id: 1, name: "Básico", price: 399, users: 1, companies: 10, workers: 100, maxUsers: 4, maxCompanies: 40, maxWorkers: 400, nextPlanPrice: 1749 },
@@ -115,7 +115,7 @@ const formatCurrency = (amount) => {
 // Cálculo del precio total incluyendo add-ons
 const totalPrice = computed(() => {
   if (!selectedPlan.value) return 0;
-  const extraHistoriesCost = extraHistories.value * 7;
+  const extraHistoriesCost = extraHistories.value * 8;
   return selectedPlan.value.price + extraHistoriesCost;
 });
 // const totalPrice = computed(() => {
@@ -517,7 +517,7 @@ const porcentajeHistorias = computed(() => {
         <h2 class="text-2xl font-semibold mb-4 text-gray-700">Extras para tu Plan</h2>
         <div class="flex flex-col gap-6 text-base">
           <div>
-            <label class="text-sm md:text-base block mb-2 text-gray-600">Aumenta tus recursos ($175 por cada 25 historias clínicas)</label>
+            <label class="text-sm md:text-base block mb-2 text-gray-600">Aumenta tus recursos ($200 por cada 25 historias clínicas)</label>
             <div class="flex items-center gap-2">
               <button @click="extraHistories > 0 ? extraHistories -= 25 : null" class="w-10 h-10 bg-gray-200 hover:bg-gray-300 flex items-center justify-center rounded transition-all duration-200">
                 <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
